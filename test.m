038df125cf0c122c817fecf69f29509e598a001a
Xavier Agostini <xavier.agostini@mail.mcgill.ca>
2015-11-02 14:51:50 -0500

    draw shape on videoFrame

--- test.m
+++ test.m
@@ -7,29 +7,22 @@ videoWidth = video.Width;
 s = struct('cdata',zeros(videoHeight,videoWidth,3,'uint8'),'colormap',[]);
 
 % Read one frame at a time
+
 k=1;
+
 while hasFrame(video)
     s(k).cdata = readFrame(video);
-    k=k+1;
-end
-
-% get attributes of s
-%whos s
-
-% Create Axes
-currAxes = axes;
-while hasFrame(video)
     videoFrame = readFrame(video);
-    image(videoFrame, 'Parent', currAxes);
-    currAxes.Visible = 'off';
-    pause(1/video.FrameRate);
-    rect = getrect;
-end
+    %Draw circle on to each frame
+    RGB = insertShape(videoFrame, 'circle', [150 280 35], 'LineWidth', 5);
+  % resize figure to video height and width
+    imshow(RGB);
 
-% resize figure to video height and width
-%set(gcf,'position',[150 150 video.Width video.Height]);
-%set(gca,'units','pixels');
-%set(gca,'position',[0 0 video.Width video.Height]);
+   % pause(1/video.FrameRate);
+
+    k=k+1;
+    
+end
 
 %play video
-movie(s,1,video.FrameRate);
+%movie(s,1,video.FrameRate);
