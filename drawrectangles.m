2885b26aa4e6100a4ab2bb1b113d9cbc19028380
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2015-11-04 01:52:32 -0500

    Add clickling logic, encapsulate rectangle selector in function

---
+++ drawrectangles.m
@@ -0,0 +1,100 @@
+function [ rect ] = drawrectangles( videoPath )
+%drawrectangles: This function returns bounded rectangles from images
+%   rect: A nx4 matrix where 
+%       n is the number of frames
+%       Each row is the rectangle selection
+%   videoPath: The location of the video in the system
+
+% This function will iterate through the frames of the video sequence
+%   For each frame you are given three options
+%       1) Draw a rectangle around a point of interest using the mouse
+%           This rectangle will be saved in the array only if it is a valid selection
+%               A valid selection will be completely inside the image
+%               and larger than a click in enclosed area (set at 6 pixels)
+%           The user will have to continue selecting 
+%               until a valid selection is made, or (2) or (3) happens
+%       2) Click to advance frames
+%           Clicks outside of the image will advance multiple frames
+%               The number of frames advanced depends 
+%               on the number of successive clicks outside the image
+%           A click on the image will reset the counter and advance one frame
+%           Skipped frames will have rects of (0,0,0,0)
+%       3) Close the image viewer to terminate the loop
+%           All subsequent frames will have rects of (0,0,0,0)
+
+
+% Instantiate the video reader
+v = VideoReader(videoPath);
+
+% Get the number of frames, frame width, and frame height from the video data
+nFrames = v.Duration * v.FrameRate;
+width = v.Width;
+height = v.Height;
+
+% Create an empty array
+rect = zeros(nFrames,4);
+
+% Instantiate the counters used in easy frame skipping
+nClicks = 0;
+nFrameSkip = 0;
+nSkips = 0;
+
+% Iterate through the image frames
+for i = 1:nFrames
+    % Skip a set number of frames
+    % This number is set below through "clicks"
+    if (nSkips < nFrameSkip)
+        nSkips = nSkips + 1;
+        continue;
+    else
+        % Reset the frame skipping indices
+        nSkips = 0;
+        nFrameSkip = 0;
+
+        try
+            % Show the image with the frame number as title
+            video = read(v,i);
+            imshow(video);
+            imgTitle = sprintf('frame %d of %d', i, nFrames);
+            title(imgTitle);
+
+            % Want to avoid selections outside of range of image
+            % This avoids such selections if they do not correspond to "clicks"
+            % Will force the user to continue selecting until valid selection is made
+            curRect = getrect;
+            while ((curRect(1) < 0 || curRect(1)+curRect(3) > width ...
+                        || curRect(2) < 0 || curRect(2)+curRect(4) > height) ...
+                        && curRect(3)+curRect(4) > 5)
+                curRect = getrect;
+            end
+
+            % If the rect is sufficiently small it corresponds to a click 
+            %   These are not used in rectangle selection
+            %   They are only used to increment the frame number
+            %   The drawn rectangles are not saved
+            if (curRect(3)+curRect(4) < 5)
+                % A click outside of the image will skip multiple frames
+                %   This number of frames is 2^number of successive clicks
+                % A click on the image will reset the counter 
+                %   It will also advance one frame
+                if (curRect(1) < 0 || curRect(1) > width ...
+                        || curRect(2) < 0 || curRect(2) > height)
+                    nClicks = nClicks + 1;
+                    nFrameSkip = 2^nClicks;
+                else
+                    nClicks = 0;
+                end
+            else
+                % A selection will reset the nClicks
+                nClicks = 0;
+                
+                % This is a valid selection, so add it to the rectangle array
+                rect(i,:) = curRect;            
+            end
+        catch
+            % If you close the image without a selection, the for loop terminates
+            break;
+        end     
+    end
+end
+end
--- test.m
+++ test.m
@@ -1,3 +1,4 @@
+
 % Load video and get its height and width
 %video = VideoReader('xylophone.mp4');
 %videoHeight = video.Height;
@@ -6,47 +7,5 @@
 % Matlab video stucture
 %s = struct('cdata',zeros(videoHeight,videoWidth,3,'uint8'),'colormap',[]);
 
-% Instantiate the video reader
-v = VideoReader('xylophone.mp4');
 
-% Get the frames from the video data
-nFrames = v.Duration * v.FrameRate
-
-% Create an empty array
-rect = zeros(nFrames,4);
-
-% Iterate through the image frames, if the image is closed before selection
-% loop will terminate
-for i = 1:nFrames
-    try
-        video = read(v,i);
-        imshow(video);
-        imgTitle = sprintf('frame %d of %d', i, nFrames);
-        title(imgTitle);
-        curRect = getrect;
-        
-        % if the rect is small it corresponds to a click, so don't save
-        if (curRect(3)+curRect(4) < 5)
-            % Can give option to skip multiple or go back depending on
-            % whether the click is outside the frame or not
-            % For instance, if the click x location is greater than the bounds of the
-            % image it corresponds to a skip of 5 images
-            % If click is to left of image (x < 0) then go back 1 image
-            % Click on image is for a one frame skip
-            % This would be basic though, might want more intuitive
-            % solution
-        else
-            % Need some error handling here... 
-            %   i.e. if the index of the rectangle is out of the img bounds
-            %   Could simply decrement i such that they have to select
-            %   again
-            rect(i,:) = curRect;
-        end
-    catch
-        % If you close the image without selection, the for loop terminates
-        break;
-    end
-end
-
-
-rect
+drawrectangles('xylophone.mp4')
