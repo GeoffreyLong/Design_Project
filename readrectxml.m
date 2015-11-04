e0b5214118452949ec5b91e56c3a2385affec594
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2015-11-04 11:59:35 -0500

    Add function to read rect object from xml

---
+++ readrectxml.m
@@ -0,0 +1,27 @@
+function [ rect ] = readrectxml( videoPath )
+%readrectxml: Will read a previously saved rectangles array for a given video
+%   rect: A nx4 matrix where 
+%       n is the number of frames
+%       Each row is the rectangle selection
+%   videoPath: The location of the video in the system
+
+% This function will read in a videoPath and will return a rectangle array
+%   The rectangle corresponds to previously saved values for the video
+%   Please note that the name of the video path should be kept consistent
+%       If it is not kept consisitent then we can run into data issues
+
+% Split the filepath so that it does not contain ".mp4"
+videoPathTokens = strsplit(videoPath,'.');
+filePath = videoPathTokens(1);
+fileName = char(filePath);
+fileName = strcat(fileName, '.dat');
+
+try
+    rect = csvread(fileName);
+catch
+    printString = sprintf('No file found by name "%s"', fileName);
+    display(printString)
+    rect = zeros(1,4);
+end
+    
+end
