1691b1b2ce42134c6f1998c8afa4239008a54011
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-02-11 18:05:15 -0500

    Some changes

--- testData/runRectangleDrawer.m
+++ testData/runRectangleDrawer.m
@@ -1,8 +1,8 @@
-filePath = 'testData/Feb_13_cam1_5.avi';
+%filePath = 'testData/Feb_13_cam1_5.avi';
 %filePath = 'testData/July_6_cam1_01.avi';
 %filePath = 'testData/July_8_cam1_01.avi';
 %filePath = 'testData/July_8_cam1_02.avi';
-%filePath = 'testData/July_8_cam1_03.avi';
+filePath = 'testData/July_8_cam1_03.avi';
 %filePath = 'testData/July_8_cam1_04.avi';
 %filePath = 'testData/July_8_cam1_08.avi';
 %filePath = 'testData/Oct_20_cam3_07.avi';
@@ -13,4 +13,3 @@ rect = drawwithhelper(filePath)
 readRect = readrectxml(filePath);
 writeRect = averagerects(rect, readRect)
 writerectxml(filePath, writeRect)
-    
