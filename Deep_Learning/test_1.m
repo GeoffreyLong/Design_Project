7c8d0f26654fc55e99a2fc9d6601e1fb52170493
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-02-17 11:29:31 -0500

    Add initial deep learning implementation / test

---
+++ Deep_Learning/test_1.m
@@ -0,0 +1,37 @@
+% setup MatConvNet
+run  'matconvnet-1.0-beta18/matlab/vl_setupnn'
+
+% load the pre-trained CNN
+net = load('nets/imagenet-vgg-f.mat') ;
+
+imagefiles = dir('Generated_Detections/truth_images/*.png');      
+nfiles = length(imagefiles);    % Number of files found
+result = zeros(1,2)
+for ii=1:nfiles
+    % load and preprocess an image
+   currentfilename = imagefiles(ii).name;
+   im = imread(currentfilename);
+    if size(im,3) == 1 % Grayscale image
+        im = cat(3, im, im, im) ;
+    end
+    im_ = single(im) ; % note: 0-255 range
+    im_ = imresize(im_, net.meta.normalization.imageSize(1:2)) ;
+    %size(im_)
+    %size(net.meta.normalization.averageImage)
+    im_ = im_ - net.meta.normalization.averageImage ;
+
+    % run the CNN
+    res = vl_simplenn(net, im_) ;
+
+    % show the classification result
+    scores = squeeze(gather(res(end).x)) ;
+    [bestScore, best] = max(scores) ;
+    %figure(1) ; clf ; imagesc(im) ;
+    %title(sprintf('%s (%d), score %.3f',...
+    %net.meta.classes.description{best}, best, bestScore)) ;
+    %sprintf('%s (%d), score %.3f',net.meta.classes.description{best}, best, bestScore)
+    result = [result; best bestScore];
+end
+
+result
+%result = sortrows(result,1)
