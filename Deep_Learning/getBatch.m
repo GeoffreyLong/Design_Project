6569bebd393294af7a5323bf8ae1fbf3f4d8cbe7
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-02-17 22:14:42 -0500

    Remove unneeded files

--- Deep_Learning/getBatch.m
+++
@@ -1,7 +0,0 @@
-function [im, labels] = getBatch(imdb, batch)
-%GETBATCH Summary of this function goes here
-%   Detailed explanation goes here
-
-    im = imdb.images.data;
-    labels = imdb.images.labels;
-end
