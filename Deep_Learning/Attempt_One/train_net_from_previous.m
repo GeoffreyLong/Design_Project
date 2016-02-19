3173b07bd74b3aab4a6eaf941f455732657604c4
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-02-19 18:20:16 -0500

    Integrate the testing and running

--- Deep_Learning/Attempt_One/train_net_from_previous.m
+++ Deep_Learning/Attempt_One/train_net_from_previous.m
@@ -1,8 +1,8 @@
 function [net, info] = train_net_from_previous()
     %%%%% Instantiate all the opt data for cnn_train %%%%%
-    opts.numEpochs = 250;
+    opts.numEpochs = 5;
     % Set logarithmic learning rate for 250 epochs
-    opts.learningRate = logspace(-3, -5, 250);
+    opts.learningRate = logspace(-3, -5, opts.numEpochs);
     % Set the batch size
     opts.batchSize = 256;
 
@@ -14,10 +14,19 @@ function [net, info] = train_net_from_previous()
     opts.train = find(imdb.images.set == 1);
     opts.val = find(imdb.images.set == 2);
 
-    [net, info] = cnn_train(net, imdb, @getBatch, opts)
+    %%%%% Train the new net %%%%%
+    [newNet, info] = cnn_train(net, imdb, @getBatch, opts);
+    newNet.layers{end}.type = 'softmax';
+   
+    %run_detector(newNet);
+    run_detector(newNet);
 end
 
 function [im, labels] = getBatch(imdb, batch)
     im = imdb.images.data(:,:,:,batch) ;
     labels = imdb.images.labels(1,batch) ;
+    
+    %TODO 
+    % Can add in jittering and other filters to add extra test data
+    % Also consider pulling out the mean of the images
 end
