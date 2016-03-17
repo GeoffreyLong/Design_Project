4c584c0a7d28692a1f07bf55454360224455eed1
Xavier Agostini <xavier.agostini@mail.mcgill.ca>
2016-03-16 21:42:46 -0400

    add to tracking

--- Algorithm_Two/tracking.m
+++ Algorithm_Two/tracking.m
@@ -1,7 +1,14 @@
-function [ outputVector ] = tracking(rect, srt)
+function [ outputVector ] = tracking(rect, host, target)
 %   tracking function should take in a detection and perform
 %   operations on it, and output results to a seperate file for 
 %   further analysis
+
+    x = rect(1)+0.5*rect(3);
+    y = rect(2)+0.5*rect(4);
+    
+    midpoint = [x y];
+    
+%     now write to external file
 
 
 
