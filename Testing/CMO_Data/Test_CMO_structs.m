aba8fca5c09ef63d4b385e29db146e744361d01f
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-04-03 18:26:25 -0400

    Update for second round of testing

--- Testing/CMO_Data/Test_CMO_structs.m
+++ Testing/CMO_Data/Test_CMO_structs.m
@@ -7,16 +7,27 @@ filePath = '/home/geoffrey/Dropbox/Temps/Design_Project/testData/July_8_cam1_02.
 % Instantiate the video reader
 v = VideoReader(filePath);
 nFrames = v.NumberOfFrames;
-readRect = readrectxml(filePath);
+readRect = readrectxml(filePath, 'tightBound_');
 
 % Type of neighborhood for CMO
 %TODO May want to experiment with other nhoods (esp diff sizes)
 %TOLEARN Which nHood gets best response
-nHoods = [strel('disk', 75), strel('diamond', 75), ...
-    strel('disk', 100), strel('diamond', 100)];
-nHoodNames = {'disk75','diamond75','disk100','diamond100'};
-thresholds = [0.10 0.12 0.15];
-nHoodDilate = ones(3);
+nHoods = [strel('disk', 1), strel('diamond', 1), strel('rect',[1,3]),...
+    strel('disk', 2), strel('diamond', 2), strel('rect',[3,1]), ...
+    strel('disk', 3), strel('diamond', 3), strel('rect',[2,3]),...
+    strel('disk', 5), strel('diamond', 5), strel('rect',[3,2]),...
+    strel('disk', 10), strel('diamond', 10), strel('rect',[3,5]),...
+    strel('disk', 25), strel('diamond', 25), strel('rect',[5,3]),...
+    strel('disk', 100), strel('diamond', 100), strel('rect',[4,10])];
+nHoodNames = {'disk1','diamond1','rect[1,3]', ...
+    'disk2','diamond2','rect[3,1]', ...
+    'disk3','diamond3','rect[2,3]', ...
+    'disk5','diamond5','rect[3,2]', ...
+    'disk10','diamond10','rect[3,5]', ...
+    'disk25','diamond25','rect[5,3]', ...
+    'disk100','diamond100','rect[4,10]'};
+thresholds = [0.07 0.09 0.11 0.13 0.15];
+% nHoodDilate = ones(3,3);
 
 
 fileID = fopen('exp_cmo.txt','w');
@@ -55,7 +66,9 @@ for j = 1:size(thresholds,2)
             plane = 0;
             for l=1:numel(s)
                 newBound = s(l).BoundingBox;
-                if (bboxOverlapRatio(newBound,curRect(2:5)))
+                
+                % Value of 0.50 arbitrarily set
+                if (bboxOverlapRatio(newBound,curRect(2:5)) >= 0.50)
                     plane = 1;
                 end
                 total = total + 1;
