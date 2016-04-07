96cf0c9e59445281f3846af496837b1b96dbcc65
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-04-07 01:18:30 -0400

    Add detection metrics test file

--- Testing/run_TestEvaluator.m
+++ Testing/run_TestEvaluator.m
@@ -1,28 +1,28 @@
 % This script runs the tests for a given testing instance or instances
 % Want to pass a folder to the test evaluator function
-folderName = '20160406T201401';
+folderName = '20160406T203250';
 
-% Set the name of the specific directory and get the files from it
+% Set the name of the specific directory and get the directories from it
 testFileBase = strcat('../Testing/Test_Instances/',folderName,'/')
 d = dir(testFileBase);
-isub = [d(:).isdir]; %# returns logical vector
+isub = [d(:).isdir];
 videoDirectories = {d(isub).name}';
 videoDirectories(ismember(videoDirectories,{'.','..'})) = [];
 
-opt_truth = [];
-scramble_truth = [];
-host = [];
-target = [];
-
-nFrames = 0;
-height = 0;
-width = 0;
-
-detections = [];
-timing = [];
-
-% Loop over the files in the directory
+% Loop over the video directories in the super directory
 for i=1:numel(videoDirectories)
+    opt_truth = [];
+    scramble_truth = [];
+    host = [];
+    target = [];
+
+    nFrames = 0;
+    height = 0;
+    width = 0;
+
+    detections = [];
+    timing = [];
+
     video = videoDirectories{i}
 
     newFileBase = strcat(testFileBase, video, '/')
@@ -52,27 +52,30 @@ for i=1:numel(videoDirectories)
         elseif strcmp(fileName,'ttc.dat')
 
         elseif strcmp(fileName,'timing.dat')
-            timing = csvread(strcat(testFileBase,fileName));
+            timing = csvread(strcat(newFileBase,fileName));
         end     
     end
-end
-% Make a results folder
-mkdir(strcat('../Testing/Test_Instances/', folderName, '/'), 'Results');
-resultFileBase = strcat('../Testing/Test_Instances/', folderName, '/Results/');
+    
+    % Make a results folder
+    mkdir(strcat('../Testing/Test_Instances/', folderName, '/', video, '/'), 'Results');
+    resultFileBase = strcat('../Testing/Test_Instances/', folderName, '/', video, '/', '/Results/');
 
-% These next if statements test the detection results against the truth files
-if (~isempty(detections) && ~isempty(opt_truth))
-    test_DetectionVSRects(strcat(resultFileBase,'optimized_'), nFrames, detections, opt_truth);
-end
-if (~isempty(detections) && ~isempty(scramble_truth))
-    %TODO some false positives in this case might not actually be an issue
-    test_DetectionVSRects(strcat(resultFileBase,'scrambled_'), nFrames, detections, scramble_truth);    
-end
+    % These next if statements test the detection results against the truth files
+    if (~isempty(detections) && ~isempty(opt_truth))
+        test_DetectionVSRects(strcat(resultFileBase,'optimized_'), nFrames, detections, opt_truth);
+    end
+    if (~isempty(detections) && ~isempty(scramble_truth))
+        %TODO some false positives in this case might not actually be an issue
+        test_DetectionVSRects(strcat(resultFileBase,'scrambled_'), nFrames, detections, scramble_truth);    
+    end
 
-% This will gather metrics such as 
-%   distance of first sighting
-%   estimated time to collision of first sighting 
-%   timing metrics
-if (~isempty(detections) && ~isempty(opt_truth) && ~isempty(target) && ~isempty(timing))
-    test_DetectionMetrics(resultFileBase, detections, opt_truth, target, timing);
+    %NOTE not sure if this file is even necessary...
+    % This will gather metrics such as 
+    %   first sightings
+    %       estimated time to collision of first sighting
+    %       distance of first sighting
+    %   timing metrics
+    if (~isempty(detections) && ~isempty(opt_truth) && ~isempty(target) && ~isempty(timing))
+        test_DetectionMetrics(resultFileBase, nFrames, detections, opt_truth, target, timing);
+    end
 end
---
+++ Testing/test_DetectionMetrics.m
@@ -0,0 +1,81 @@
+function test_DetectionMetrics( resultFileBase, nFrames, rect, truth, target, timing )
+%TEST_DETECTIONMETRICS Summary of this function goes here
+%   
+
+
+    
+    % Where a plane first appears in the generated files
+    firstTruthDetection = [];
+    % Where a plane first appears in the detections
+    firstRectDetection = [];
+    % Where the first unbroken string of 4+ detections occurs
+    %   Note: the number 4+ rather arbitrarily chosen
+    firstDetectionString = [];
+    continuityCount = 0;
+    stringThreshold = 4;
+    
+    for i = 1:nFrames
+        curTruth = truth(truth(:,1)==i,:);
+        curRect = rect(rect(:,1)==i,:);
+        
+        if (size(curTruth,1) ~= 0 && isempty(firstTruthDetection))
+            firstTruthDetection = curTruth(1,:);
+        end
+
+        % Iterate over the detections found for the frame
+        for j = 1:size(curRect,1)
+            tempRect = curRect(j,:);
+
+            % foundPlane checks to see if a plane was found
+            foundPlane = 0;
+
+            % Iterate over the actual detections in the frame
+            for k = 1:size(curTruth,1)
+                tempTruth = curTruth(k,:);
+
+                % If the ratio of the minimum is 1, 
+                % then one rect is completely within another rect.
+                % This is a valid detection
+                if (bboxOverlapRatio(tempRect(2:5), tempTruth(2:5), 'ratioType', 'min') == 1)
+                    foundPlane = 1;
+                    if (isempty(firstRectDetection))
+                        firstRectDetection = tempRect;
+                    end
+                end
+            end 
+
+            % If there was a plane found for the rect, then true pos
+            if (foundPlane == 1)
+                continuityCount = continuityCount + 1;
+                if continuityCount >= stringThreshold ...
+                        && isempty(firstDetectionString)
+                    firstDetectionString = tempRect;
+                end
+            else
+                continuityCount = 0;
+            end
+        end
+    end
+
+    
+    firstDetectionString(1) = firstDetectionString(1) - stringThreshold;
+    firstTruthDetection
+    firstRectDetection
+    firstDetectionString
+
+    
+    fileID = fopen(strcat(resultFileBase, 'detectionMetrics.txt'), 'w');
+
+    fprintf(fileID, 'Number of frames between \n');
+    fprintf(fileID, '\t First truth detection and detection: \t %s \n', firstRectDetection - firstTruthDetection);
+    fprintf(fileID, '\t First truth detection and 4+ detection string: \t %s \n', firstDetectionString - firstTruthDetection);
+    fprintf(fileID, '\t First detection and 4+ detection string: \t %s \n\n', firstDetectionString - firstRectDetection);
+
+    fprintf(fileID, 'Distance of plane at \n');
+    fprintf(fileID, '\t First truth detection: \t %s (ft) \n', target(firstTruthDetection(1)));
+    fprintf(fileID, '\t First detection: \t %s (ft) \n', target(firstRectDetection(1)));
+    fprintf(fileID, '\t First 4+ detection string: \t %s (ft) \n', target(firstDetectionString(1)));
+        
+    fclose(fileID);
+end
+
